8fdf11d5ab806ee0620977f48dd8cc47a47e2a46
Alexis <alex.p.flores17@gmail.com>
2021-01-24 23:05:38 -0600

    footer

--- src/Componentes/Footer.js
+++ src/Componentes/Footer.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
 
 
 
 export const Footer = () =>{
 
     return(
-        <div className="Footer">
-            Soy un footer
-        </div>
+            <Container className="footerContainer">
+                <div className="copyOxigeno">&copy; Oxigeno.cc</div>
+                <div className="refLsd"><a href="https://lsdlab.com.mx/" target="_blank">Administrado por Light & Sound Disruptive Lab</a></div>
+            </Container>
     );
 }
