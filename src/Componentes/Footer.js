8a87f6c1f63363ee9f4b06b152250a0a5921ed66
Alexis <alex.p.flores17@gmail.com>
2021-01-24 23:30:50 -0600

    footer

--- src/Componentes/Footer.js
+++ src/Componentes/Footer.js
@@ -7,9 +7,9 @@ import Container from 'react-bootstrap/Container';
 export const Footer = () =>{
 
     return(
-            <Container className="footerContainer">
-                <div className="copyOxigeno">&copy; Oxigeno.cc</div>
-                <div className="refLsd"><a href="https://lsdlab.com.mx/" target="_blank">Administrado por Light & Sound Disruptive Lab</a></div>
-            </Container>
+        <div className="footerContainer">
+            <div className="copyOxigeno">&copy; Oxigeno.cc</div>
+            <div className="refLsd"><a href="https://lsdlab.com.mx/" target="_blank">Administrado por Light & Sound Disruptive Lab</a></div>
+        </div>
     );
 }
