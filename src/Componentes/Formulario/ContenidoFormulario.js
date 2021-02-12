415a83da94b69af3f56f59f8673bab5aa3530c1a
Alexis <alex.p.flores17@gmail.com>
2021-02-12 00:15:59 -0600

    Diseño Formulario v1

--- src/Componentes/Formulario/ContenidoFormulario.js
+++ src/Componentes/Formulario/ContenidoFormulario.js
@@ -30,7 +30,7 @@ export const ContenidoFormulario = () => {
         <div className="formularioContainer">
             <div className="formularioTitulo">Formulario de registro para distribuidores</div>
             <div className="formulariocontenido">
-                <div className="formularioFormulario">
+                <div className="formularioFormulario col-8">
                     <form>
                         <div className="form-group formularioNombre">
                             <label htmlFor="nombreDistribuidor">Nombre de la empresa</label>
@@ -38,10 +38,14 @@ export const ContenidoFormulario = () => {
                         </div>
 
                         <div className="form-group formularioRFCTel">
-                            <label htmlFor="rfc">RFC</label>
-                            <input type="text" className="form-control" placeholder="RFC" name="rfc" onChange={handleInputChange} />
-                            <label htmlFor="telefono">Telefono</label>
-                            <input type="text" className="form-control" placeholder="Telefono" name="telefono" onChange={handleInputChange} />
+                            <div className="formularioRFC">
+                                <label htmlFor="rfc">RFC</label>
+                                <input type="text" className="form-control" placeholder="RFC" name="rfc" onChange={handleInputChange} />
+                            </div>
+                            <div className="formularioTel">
+                                <label htmlFor="telefono">Telefono</label>
+                                <input type="text" className="form-control" placeholder="Telefono" name="telefono" onChange={handleInputChange} />
+                            </div>
                         </div>
 
                         <div className="form-group formularioDireccion">
@@ -55,42 +59,50 @@ export const ContenidoFormulario = () => {
                         </div>
 
                         <div className="form-group formularioWebWhats">
-                            <label htmlFor="linkPagina">Página web</label>
-                            <input type="text" className="form-control" placeholder="Página web" name="linkPagina" onChange={handleInputChange} />
-                            <label htmlFor="whatsapp">Whatsapp</label>
-                            <input type="text" className="form-control" placeholder="Whatsapp" name="whatsapp" onChange={handleInputChange} />
+                            <div className="formularioWeb">
+                                <label htmlFor="linkPagina">Página web</label>
+                                <input type="text" className="form-control" placeholder="Página web" name="linkPagina" onChange={handleInputChange} />
+                            </div>
+                            <div className="formularioWhats">
+                                <label htmlFor="whatsapp">Whatsapp</label>
+                                <input type="text" className="form-control" placeholder="Whatsapp" name="whatsapp" onChange={handleInputChange} />
+                            </div>
                         </div>
 
                         <div className="form-group formularioDomicilioTarjeta">
                          
                             <div className="form-check formularioDomicilio" >
-                                <label htmlFor="aDomicilio">Servicio a domicilio</label>
-                                <span>¿Ofreces servicio a domicilio?</span><input className="form-check-input" type="checkbox" type="radio" name="aDomicilio" onChange={handleInputChange} />
+                                <div><label htmlFor="aDomicilio">Servicio a domicilio</label></div>
+                                <div className="domicilioBoton">
+                                    <span>¿Ofreces servicio a domicilio?</span><input className="form-check-input" type="checkbox" type="radio" name="aDomicilio" onChange={handleInputChange} />
+                                </div>
                             </div>
 
-                            <div className="form-check formularioDomicilio" >
-                                <label htmlFor="checkbox">Pago con tarjeta</label>
-                                <span>¿Ofreces pago con tarjeta?</span><input className="form-check-input" type="checkbox" value={formValues.pagoConTarjeta}  type="radio" name="pagoConTarjeta" onChange={handleInputChange} />
+                            <div className="form-check formularioTarjeta" >
+                                <div><label htmlFor="pagoConTarjeta">Pago con tarjeta</label></div>
+                                <div className="tarjetaBoton">
+                                    <span>¿Ofreces pago con tarjeta?</span><input className="form-check-input" type="checkbox" type="radio" name="pagoConTarjeta" onChange={handleInputChange} />
+                                </div>
                             </div>
                         </div>
 
                         <div className="form-group formularioTanquesConcentradores">
                          
                             <div className="form-check formularioTanques" >
-                                <label htmlFor="ofreceVentaDeTanque">Tanques</label>
-                                <span>Venta de tanques</span><input className="form-check-input" type="checkbox" value={formValues.ofreceVentaDeTanque}  type="radio" name="ofreceVentaDeTanque" onChange={handleInputChange} />
-                                <span>Renta de tanques</span><input className="form-check-input" type="checkbox" value={formValues.ofreceRentaDeTanque}  type="radio" name="ofreceRentaDeTanque" onChange={handleInputChange} />
-                                <span>Recarga de tanques</span><input className="form-check-input" type="checkbox" value={formValues.ofreceRecargaDeTanque}  type="radio" name="ofreceRecargaDeTanque" onChange={handleInputChange} />
+                                <div><label htmlFor="ofreceVentaDeTanque">Tanques</label></div>
+                                <div className="botonTanques"><span>Venta de tanques</span><input className="form-check-input" type="checkbox"   type="radio" name="ofreceVentaDeTanque" onChange={handleInputChange} /></div>
+                                <div className="botonTanques"><span>Renta de tanques</span><input className="form-check-input" type="checkbox"  type="radio" name="ofreceRentaDeTanque" onChange={handleInputChange} /></div>
+                                <div className="botonTanques"><span>Recarga de tanques</span><input className="form-check-input" type="checkbox"  type="radio" name="ofreceRecargaDeTanque" onChange={handleInputChange} /></div>
                             </div>
 
                             <div className="form-check formularioConcentradores" >
                                 <label htmlFor="ofreceVentaDeConcentradores">Concentradores</label>
-                                <span>Venta de concentradores</span><input className="form-check-input" type="checkbox" value={formValues.ofreceVentaDeConcentrador}  type="radio" name="ofreceVentaDeConcentrador" onChange={handleInputChange} />
-                                <span>Renta de concentradores</span><input className="form-check-input" type="checkbox" value={formValues.ofreceRentaDeConcentrador}  type="radio" name="ofreceRentaDeConcentrador" onChange={handleInputChange} />
+                                <div className="botonConcentradores"><span>Venta de concentradores</span><input className="form-check-input" type="checkbox"  type="radio" name="ofreceVentaDeConcentrador" onChange={handleInputChange} /></div>
+                                <div className="botonConcentradores"><span>Renta de concentradores</span><input className="form-check-input" type="checkbox"  type="radio" name="ofreceRentaDeConcentrador" onChange={handleInputChange} /></div>
                             </div>
                         </div>
 
-                        <button type="submit" className="btn btn-primary" onClick={(e)=>submit(e)}>Confirmar</button>
+                        <div className="botonSubmit"><button  type="submit" className="btn btn-primary" onClick={(e)=>submit(e)}>Confirmar</button></div>
                     </form> 
                 </div>
             </div>
