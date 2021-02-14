1bc487c5d27e95c7739dcc017f4c030e0d76bbfd
Alexis <alex.p.flores17@gmail.com>
2021-02-13 21:13:37 -0600

    update

--- src/types/endPoints.js
+++ src/types/endPoints.js
@@ -1,9 +1,4 @@
 
 
-<<<<<<< HEAD
-export const endPoints = "https://dev-oxigeno.cdmx.gob.mx/oxigeno/";
-// export const endPoints =  window.location.href;
-=======
 // export const endPoints = "https://dev-oxigeno.cdmx.gob.mx/oxigeno/";
 export const endPoints =  window.location.href;
->>>>>>> main
