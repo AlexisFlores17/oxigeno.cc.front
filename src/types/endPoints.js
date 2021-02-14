32afc5315827a22618bd0ef4fc891314f4a8c6c8
Alexis <alex.p.flores17@gmail.com>
2021-02-14 00:48:18 -0600

    login

--- src/types/endPoints.js
+++ src/types/endPoints.js
@@ -1,4 +1,4 @@
 
 
-// export const endPoints = "https://dev-oxigeno.cdmx.gob.mx/api/v2/";
-export const endPoints =  `${window.location.href}api/v2/`;
+export const endPoints = "https://dev-oxigeno.cdmx.gob.mx/api/v2/";
+// export const endPoints =  `${window.location.href}api/v2/`;
