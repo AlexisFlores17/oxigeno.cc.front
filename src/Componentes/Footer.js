b1ab1cecf639ea76ef1a4d37badd156b4a2a654a
dstprojects <36209165+dstprojects@users.noreply.github.com>
2021-02-16 01:03:56 -0600

    nuevo link postman

--- src/Componentes/Footer.js
+++ src/Componentes/Footer.js
@@ -26,7 +26,7 @@ export const Footer = () =>{
             </div>
             <div className="footerPostman">
                 <p className="textoPostman">¿Eres programador? puedes usar nuestra API de forma gratuita</p>
-                <a href="https://app.getpostman.com/run-collection/90220cc63e76e5062bfd" target="_blank"><div className="botonPostman">Pruébala en postman</div></a>
+                <a href="https://app.getpostman.com/run-collection/3700d8f9db0ba674fc55" target="_blank"><div className="botonPostman">Pruébala en postman</div></a>
             </div>
             <div className="footerLocatel">
                 <div>
