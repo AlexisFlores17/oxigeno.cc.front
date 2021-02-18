65b04e725d71d54d9b5012ae35900b5577f33ba7
dstprojects <36209165+dstprojects@users.noreply.github.com>
2021-02-17 19:07:41 -0600

    Update PaginaDistribuidor.js

--- src/Componentes/Manager/PaginaDistribuidor.js
+++ src/Componentes/Manager/PaginaDistribuidor.js
@@ -10,34 +10,38 @@ export const PaginaDistribuidor = () => {
 
     const state = useSelector( state => state.paginaReducer );
     const [dataDistribuidor, setDataDistribuidor] = useState({});
+    const [cargado, setCargado] = useState(false);
     const dispatch = useDispatch();
+
     const onClickRegresar = ()=>{
         dispatch(tablaDistribuidor());
     }
-    useEffect(() => {
-        getData();
-      },[]);
-    
-      async function getData() {
-        try {
-          const dataPeticionInicial = await axios.get(`${endPointsManager}${state.id}`,{});
-          const dataDistrib = await dataPeticionInicial.data;
-    
-          if (dataPeticionInicial.status === 200) {
-            setDataDistribuidor(dataDistrib);
-          }
 
-        } catch (error) {
-          swal("¡Ups!", "Lo sentimos, hubo un error al cargar distribuidor. Por favor intente más tarde", "error");
-          console.log(error)
+    useEffect(() => {
+      getData();
+    },[]);
+  
+    async function getData() {
+      try {
+        const dataPeticionInicial = await axios.get(`${endPointsManager}${state.id}`,{});
+        const dataDistrib = await dataPeticionInicial.data;
+  
+        if (dataPeticionInicial.status === 200) {
+          setDataDistribuidor(dataDistrib);
+          setCargado(true);
         }
+
+      } catch (error) {
+        swal("¡Ups!", "Lo sentimos, hubo un error al cargar distribuidor. Por favor intente más tarde", "error");
+        console.log(error)
       }
+    }
     
 
 
     return (
         <div>
-            Hola soy Distribuidor
+            { cargado ? dataDistribuidor.nombre_distribuidor : <div>Cargando</div>} <br></br>
             <button className="btn btn-primary" onClick={()=>onClickRegresar()}>Regresar</button>
 
         </div>
